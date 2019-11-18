9dff1163befaf6937f943ce6e2747d318b039db5
Rupank Bansal <rupank@playpowerlabs.com>
2019-11-18 14:40:53 +0530

    Added Task Implementation with styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import debounce from 'lodash.debounce';
+import uniq from 'lodash.uniq';
+import intersection from 'lodash.intersection';
+import union from 'lodash.union';
 import './App.css';
 
 function App() {
+
+  const [numbers, setNumbers] = useState([]);
+  const [duplicates, setDuplicates] = useState([]);
+  const [inputEntries, setInputs] = useState([]);
+
+  const KeyupEvent = debounce((input) => {
+    let pattern = /^\d+$/;
+    let inputs = input.split(",").map(item => item.trim());
+    inputs.forEach(element => {
+      let isFound = false;
+      inputEntries.forEach(val => {
+        if (val === element) isFound = true;
+      })
+      if (!isFound) {
+        setInputs([...inputs, element]);
+        let rangeList = element.split("-").map(item => parseInt(item.trim())).filter(item =>
+          pattern.test(item) === true);
+        let start = rangeList[0];
+        let end;
+        if (rangeList.length > 1) {
+          end = rangeList[1];
+          checkDuplicateWithRange(start, end);
+        }
+        else {
+          start && checkDuplicateNumber(start);
+        }
+      }
+
+    });
+  }, 500);
+
+
+
+  function setStateValues(arr) {
+    let duplicateArray = intersection(numbers, arr);
+    let unionArray = union(numbers, arr);
+    let uniqDuplicatedArray = uniq([...duplicates, ...duplicateArray]);
+    let uniqMainArray = uniq([...numbers, ...unionArray]);
+    setDuplicates(uniqDuplicatedArray);
+    setNumbers(uniqMainArray);
+  }
+
+  function checkDuplicateNumber(val) {
+    setStateValues([val]);
+  }
+  function checkDuplicateWithRange(start, end) {
+    let temp_arr = [];
+    for (let i = start; i <= end; i++) {
+      temp_arr.push(i);
+    }
+    setStateValues(temp_arr);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      Type Here:
+        <input type="text" onKeyUp={(e) => KeyupEvent(e.target.value)}></input>
+      <div className="container">
+        <div >
+          Elements in Array are - {numbers.join(",")}
+        </div>
+        <div>
+          Duplicates are - {duplicates.join(",")}
+        </div>
+      </div>
+    </div >
   );
 }
 

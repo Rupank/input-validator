a71a237bad872e2470ca46548094751965338695
Rupank Bansal <rupank@playpowerlabs.com>
2019-11-18 19:59:27 +0530

    Removed Unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './AppHook';
 import App from './AppClass';
 
 ReactDOM.render(<App />, document.getElementById('root'));
